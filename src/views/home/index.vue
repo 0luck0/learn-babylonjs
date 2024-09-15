1cf841f5be81a5ed559b0291fedb39ef2fe658cf
yuan <2368130197@qq.com>
2024-09-15 16:08:19 +0800

    build+

---
+++ src/views/home/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <canvas id="renderCanvas"></canvas>
+</template>
+<script lang="ts" setup>
+import { onMounted } from 'vue';
+import { AppOne as App } from './AppOne';
+
+onMounted(() => {
+    const canvasElement: HTMLCanvasElement | null = document.getElementById('renderCanvas') as HTMLCanvasElement | null;
+
+    if (canvasElement) {
+        const app = new App(canvasElement);
+        app.run();
+    } else {
+        console.error('Canvas element not found');
+    }
+
+})
+
+</script>
+<style scoped>
+#renderCanvas {
+    width: 100vw;
+    height: 100vh;
+    background-color: #000;
+    overflow: hidden;
+    outline: 0px;
+
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,31 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+// import process from 'process'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  //开发环境和生产环境
-  base:'/learn-babylonjs/',
-  plugins: [vue()],
-  server: {
-    open: true
-  },
-  build: {
-    outDir: 'docs'
+export default defineConfig(({ mode}) => {
+
+  // const env = loadEnv(mode, process.cwd(), '')
+  const isProd:boolean = mode === 'production'
+  return {
+    base:  isProd?'/learn-babylonjs/': '/',
+    plugins: [vue()],
+    resolve: {
+      alias: {
+          // @ 替代为 src
+        '@': resolve(__dirname, 'src'),
+         // @component 替代为 src/component
+        '@components': resolve(__dirname, 'src/components'),
+      },
+    },
+    server: {
+      open: true
+    },
+    build: {
+      outDir: 'docs'
+    }
   }
-   
-  
+
+
 })
