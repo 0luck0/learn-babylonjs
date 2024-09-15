037ab6b8c62a8a464dd8ac63f7b289bd081368aa
yuan <2368130197@qq.com>
2024-09-15 15:39:40 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   //开发环境和生产环境
-  base: env.NODE_ENV === 'production' ? '/learn-babylonjs/' : '/',
+  base:'/learn-babylonjs/',
   plugins: [vue()],
   server: {
-    port: 3000,
     open: true
   }
   
