54fe6bf0879f7ce968c83a8a08a73dbc0ce372b8
yuan <2368130197@qq.com>
2024-09-15 23:09:53 +0800

    update

--- src/views/home/AppOne.ts
+++ src/views/home/AppOne.ts
@@ -1,27 +1,23 @@
-import {Color3, Engine,FreeCamera,HemisphericLight,MeshBuilder,Scene, StandardMaterial, Vector3} from "@babylonjs/core"
+import { Color3, Color4,  DefaultRenderingPipeline,  Engine, FreeCamera, HemisphericLight, PointLight, Scene, SceneLoader,  Vector3 } from "@babylonjs/core"
+import "@babylonjs/loaders";
+
 export class AppOne {
-    engine:  Engine;
-    scene:  Scene;
+    engine: Engine;
+    scene: Scene;
 
     constructor(readonly canvas: HTMLCanvasElement) {
-        this.engine = new  Engine(canvas)
+        this.engine = new Engine(canvas)
         window.addEventListener('resize', () => {
             this.engine.resize();
         });
         this.scene = createScene(this.engine, this.canvas)
-
+      
     }
 
-    debug(debugOn: boolean = true) {
-        if (debugOn) {
-            this.scene.debugLayer.show({ overlay: true });
-        } else {
-            this.scene.debugLayer.hide();
-        }
-    }
+    
 
     run() {
-        this.debug(true);
+      
         this.engine.runRenderLoop(() => {
             this.scene.render();
         });
@@ -32,61 +28,53 @@ export class AppOne {
 }
 
 
-var createScene = function (engine:  Engine, canvas: HTMLCanvasElement) {
+var createScene = function (engine: Engine, canvas: HTMLCanvasElement) {
     // this is the default code from the playground:
 
     // This creates a basic Babylon Scene object (non-mesh)
-    var scene = new  Scene(engine);
-
-    // This creates and positions a free camera (non-mesh)
-    var camera = new  FreeCamera("camera1", new  Vector3(0, 5, -10), scene);
-
-    // This targets the camera to scene origin
-    camera.setTarget( Vector3.Zero());
-
-    // This attaches the camera to the canvas
+    var scene = new Scene(engine);
+    scene.createDefaultEnvironment(
+        {
+            createGround: false,
+            createSkybox: false
+        }
+    );
+    scene.clearColor = Color4.FromHexString("#ffffffff");
+    var camera = new FreeCamera("camera", new Vector3(0, 2.5, 10), scene);
+    camera.setTarget(Vector3.Zero());
     camera.attachControl(canvas, true);
+    var light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+    light.intensity = 1;
+    
+    //point light
+    var light2 = new PointLight("pointLight", new Vector3(0, 5, 0), scene);
+    light2.position = new Vector3(-5, 5, 0);
+    light2.intensity = 10;
+    light2.radius = 110;
+    light2.diffuse = new Color3(1, 0, 0);
+ 
 
-    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    var light = new  HemisphericLight("light", new  Vector3(0, 1, 0), scene);
-
-    // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 0.7;
-
-    // Our built-in 'sphere' shape.
-    var sphere =  MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
-    // Move the sphere upward 1/2 its height
-    let startPos = 2;
-    sphere.position.y = startPos;
-
-    // Our built-in 'ground' shape.
-    var ground =  MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
-    var groundMaterial = new  StandardMaterial("groundMaterial", scene);
-    groundMaterial.diffuseColor = new  Color3(0.5, 0.8, 0.5); // RGB for a greenish color
-    ground.material = groundMaterial;
-
-
-
-    var redMaterial = new  StandardMaterial("redMaterial", scene);
-    redMaterial.diffuseColor = new  Color3(0, 1, 1); // RGB for red
-    sphere.material = redMaterial;
-
-    var sphereVelocity = 0;
-    var gravity = 0.009;
-    var reboundLoss = 0.1;
-
+    var light3 = new PointLight("pointLight", new Vector3(0, 5, 0), scene);
+    light3.position = new Vector3(5, 5, 0);
+    light3.intensity = 10;
+    light3.radius = 110;
+    light3.diffuse = new Color3(0, 1, 0);
+    
+    SceneLoader.AppendAsync("", "/mesh/萌三兄弟.glb", scene).then(() => {
+        // do nothing
+    });
+    //mass
+    var pipeline = new DefaultRenderingPipeline(
+        "defaultPipeline", // The name of the pipeline
+        true, // Do you want the pipeline to use HDR texture?
+        scene, // The scene instance
+        [camera] // The list of cameras to be attached to
+    );
+    pipeline.samples = 4;
+    pipeline.fxaaEnabled = true;
+    
     scene.registerBeforeRender(() => {
-        sphereVelocity += gravity;
-        let newY = sphere.position.y - sphereVelocity;
-        sphere.position.y -= sphereVelocity
-        if (newY < 1) {
-            sphereVelocity = (reboundLoss - 1) * sphereVelocity;
-            newY = 1;
-        }
-        sphere.position.y = newY;
-        if (Math.abs(sphereVelocity) <= gravity && newY < 1 + gravity) {
-            sphere.position.y = startPos++;
-        }
+
     });
 
     return scene;
